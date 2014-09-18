7e46a34d2dc28167c565d0c7582ea17992e85a4a
zhijia,.zhang <jiahut@gmail.com>
2014-09-18 17:56:28 +0800

    update use my- as  prefix

--- personal/my-helper.el
+++ personal/my-helper.el
@@ -2,18 +2,43 @@
 ;;; Commentary:
 ;;; code:
 
-(defun print-current-major-mode (&optional buffer-or-name)
+(defun my-print-current-major-mode (&optional buffer-or-name)
 "Return the major mode associated with a buffer.
 If BUFFER-OR-NAME is nil return current buffer's mode."
   (interactive)
   (message "%s" (buffer-local-value 'major-mode
    (if buffer-or-name (get-buffer buffer-or-name) (current-buffer)))))
 
-(defun open-my-note()
+(defun my-setq-debug()
   (interactive)
-  (find-file-other-window "~/Projects/note/README.md"))
+  (setq debug-on-error t)
+)
 
-(defun open-finder-1 (dir file)
+(defun my-open-note()
+  (interactive)
+  (find-file-other-window "~/Projects/note/emacs/mode.org"))
+
+(defun my-create-scratch-buffer nil
+   "create a scratch buffer"
+   (interactive)
+   (switch-to-buffer (get-buffer-create "*scratch*"))
+   (lisp-interaction-mode))
+
+(defun my-defind-font()
+  (interactive)
+  (set-frame-font "Monaco-15")
+  ;; (set-frame-font "Consolas-18")
+  (set-face-attribute 'default nil :font "Consolas-18")
+  (set-fontset-font
+    (frame-parameter nil 'font)
+    'han
+    (font-spec :family "STHeiti" :size 15))
+    ;;(font-spec :name "WenQuanYi Micro Hei Mono"))
+    ;;(font-spec :family "DejaVu Sans Mono" :size 12))
+    ;;(font-spec :family "Hiragino Sans GB" ))
+  )
+
+(defun my-open-finder-1 (dir file)
   (let ((script
 		 (if file
 			 (concat
@@ -30,14 +55,19 @@ If BUFFER-OR-NAME is nil return current buffer's mode."
     (start-process "osascript-getinfo" nil "osascript" "-e" script)))
 
 
-(defun open-finder ()
+(defun my-open-finder ()
   (interactive)
   (let ((path (buffer-file-name))
 		dir file)
 	(when path
 	  (setq dir (file-name-directory path))
 	  (setq file (file-name-nondirectory path)))
-	(open-finder-1 dir file)))
+	(my-open-finder-1 dir file)))
+
+;; C-x C-e ;; current line
+;; M-x eval-region ;; region
+;; M-x eval-buffer ;; whole buffer
+;; M-x load-file ~/.emacs.d/init.el
 
 (provide 'my-helper)
 ;;; my-helper.el ends here
