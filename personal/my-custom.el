f2312a305ba6e5ee7b4f86a8d1fc7189039c4620
zhijia,.zhang <jiahut@gmail.com>
2014-10-31 17:16:07 +0800

    move to cycbuf

--- personal/my-custom.el
+++ personal/my-custom.el
@@ -14,7 +14,7 @@
 ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 ;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
-(prelude-require-packages '(evil evil-surround tramp thrift powerline-evil
+(prelude-require-packages '(evil evil-surround tramp thrift powerline-evil cycbuf
                                  helm-company jade-mode help-fns+ coffee-mode
                                  dirtree ag helm-ag helm-swoop
                                  smooth-scrolling indent-guide
@@ -259,58 +259,64 @@
         (push 'escape unread-command-events))
        (t (setq unread-command-events (append unread-command-events
                                               (list evt))))))))
-
-;; buffer burial
-;; necessary support function for buffer burial
-(defun crs-delete-these (delete-these from-this-list)
-  "Delete DELETE-THESE FROM-THIS-LIST."
-  (cond
-   ((car delete-these)
-    (if (member (car delete-these) from-this-list)
-        (crs-delete-these (cdr delete-these) (delete (car delete-these)
-                                                     from-this-list))
-      (crs-delete-these (cdr delete-these) from-this-list)))
-   (t from-this-list)))
-                                        ; this is the list of buffers I never want to see
-(defvar crs-hated-buffers
-  '("KILL" "*Compile-Log*"))
-                                        ; might as well use this for both
-(setq iswitchb-buffer-ignore (append '("^ " "*Buffer") crs-hated-buffers))
-(defun crs-hated-buffers ()
-  "List of buffers I never want to see, converted from names to buffers."
-  (delete nil
-          (append
-           (mapcar 'get-buffer crs-hated-buffers)
-           (mapcar (lambda (this-buffer)
-                     (if (string-match "^\*" (buffer-name this-buffer))
-                         this-buffer))
-                   (buffer-list))
-           (mapcar (lambda (this-buffer)
-                     (if (string-match "^ " (buffer-name this-buffer))
-                         this-buffer))
-                   (buffer-list)))))
-                                        ; I'm sick of switching buffers only to find KILL right in front of me
-(defun crs-bury-buffer (&optional n)
-  (interactive)
-  (unless n
-    (setq n 1))
-  (let ((my-buffer-list (crs-delete-these (crs-hated-buffers)
-                                          (buffer-list (selected-frame)))))
-    (switch-to-buffer
-     (if (< n 0)
-         (nth (+ (length my-buffer-list) n)
-              my-buffer-list)
-       (bury-buffer)
-       (nth n my-buffer-list)))))
+;; ;; ;; move to cycbuf
+;; ;; buffer burial
+;; ;; necessary support function for buffer burial
+;; (defun crs-delete-these (delete-these from-this-list)
+;;   "Delete DELETE-THESE FROM-THIS-LIST."
+;;   (cond
+;;    ((car delete-these)
+;;     (if (member (car delete-these) from-this-list)
+;;         (crs-delete-these (cdr delete-these) (delete (car delete-these)
+;;                                                      from-this-list))
+;;       (crs-delete-these (cdr delete-these) from-this-list)))
+;;    (t from-this-list)))
+;;                                         ; this is the list of buffers I never want to see
+;; (defvar crs-hated-buffers
+;;   '("KILL" "*Compile-Log*"))
+;;                                         ; might as well use this for both
+;; (setq iswitchb-buffer-ignore (append '("^ " "*Buffer") crs-hated-buffers))
+;; (defun crs-hated-buffers ()
+;;   "List of buffers I never want to see, converted from names to buffers."
+;;   (delete nil
+;;           (append
+;;            (mapcar 'get-buffer crs-hated-buffers)
+;;            (mapcar (lambda (this-buffer)
+;;                      (if (string-match "^\*" (buffer-name this-buffer))
+;;                          this-buffer))
+;;                    (buffer-list))
+;;            (mapcar (lambda (this-buffer)
+;;                      (if (string-match "^ " (buffer-name this-buffer))
+;;                          this-buffer))
+;;                    (buffer-list)))))
+;;                                         ; I'm sick of switching buffers only to find KILL right in front of me
+;; (defun crs-bury-buffer (&optional n)
+;;   (interactive)
+;;   (unless n
+;;     (setq n 1))
+;;   (let ((my-buffer-list (crs-delete-these (crs-hated-buffers)
+;;                                           (buffer-list (selected-frame)))))
+;;     (switch-to-buffer
+;;      (if (< n 0)
+;;          (nth (+ (length my-buffer-list) n)
+;;               my-buffer-list)
+;;        (bury-buffer)
+;;        (nth n my-buffer-list)))))
 ;; (global-set-key [(control tab)] 'crs-bury-buffer)
 ;; (global-set-key [(control shift tab)] (lambda ()
 ;;                                         (interactive)
 ;;                                         (crs-bury-buffer -1)))
-(global-set-key (kbd "C-x n") 'crs-bury-buffer)
-(global-set-key (kbd "C-x p") (lambda ()
-                                (interactive)
-                                (crs-bury-buffer -1)))
+;; (global-set-key (kbd "C-x n") 'crs-bury-buffer)
+;; (global-set-key (kbd "C-x p") (lambda ()
+;;                                 (interactive)
+;;                                 (crs-bury-buffer -1)))
 
+(require 'cycbuf)
+(define-key prelude-mode-map (kbd "C-x n") 'cycbuf-switch-to-next-buffer)
+(define-key prelude-mode-map (kbd "C-x p") 'cycbuf-switch-to-previous-buffer)
+
+;; (define-key prelude-mode-map (kbd "C-x n") 'next-buffer)
+;; (define-key prelude-mode-map (kbd "C-x p") 'previous-buffer)
 
 (when (display-graphic-p)
   (setq fonts
