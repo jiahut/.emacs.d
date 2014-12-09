d80cf2b8a5ae287459cc9c5aa78756c6938ce79b
zhijia,.zhang <jiahut@gmail.com>
2014-12-09 10:02:04 +0800

    add my-http-proxy and format

--- personal/my-helper.el
+++ personal/my-helper.el
@@ -15,35 +15,33 @@ Note the weekly scope of the command's precision.")
   "insert the current date and time into current buffer.
 Uses `current-date-time-format' for the formatting the date/time."
   (interactive)
-  (insert (format-time-string current-date-time-format (current-time)))
-  )
+  (insert (format-time-string current-date-time-format (current-time))))
+
 (defun my-insert-current-time ()
   "insert the current time (1-week scope) into the current buffer."
   (interactive)
-  (insert (format-time-string current-time-format (current-time)))
-  )
+  (insert (format-time-string current-time-format (current-time))))
 
 (defun my-print-current-major-mode (&optional buffer-or-name)
-"Return the major mode associated with a buffer.
+  "Return the major mode associated with a buffer.
 If BUFFER-OR-NAME is nil return current buffer's mode."
   (interactive)
   (message "%s" (buffer-local-value 'major-mode
-   (if buffer-or-name (get-buffer buffer-or-name) (current-buffer)))))
+                                    (if buffer-or-name (get-buffer buffer-or-name) (current-buffer)))))
 
 (defun my-setq-debug()
   (interactive)
-  (setq debug-on-error t)
-)
+  (setq debug-on-error t))
 
 (defun my-open-note()
   (interactive)
   (find-file-other-window "~/Projects/note/emacs/mode.org"))
 
 (defun my-create-scratch-buffer nil
-   "create a scratch buffer"
-   (interactive)
-   (switch-to-buffer (get-buffer-create "*scratch*"))
-   (lisp-interaction-mode))
+  "create a scratch buffer"
+  (interactive)
+  (switch-to-buffer (get-buffer-create "*scratch*"))
+  (lisp-interaction-mode))
 
 (defun my-defind-font()
   (interactive)
@@ -51,39 +49,50 @@ If BUFFER-OR-NAME is nil return current buffer's mode."
   ;; (set-frame-font "Consolas-18")
   ;; (set-face-attribute 'default nil :font "Consolas-18")
   (set-fontset-font
-    (frame-parameter nil 'font)
-    'han
-    (font-spec :family "STHeiti" :size 15))
-    ;;(font-spec :name "WenQuanYi Micro Hei Mono"))
-    ;;(font-spec :family "DejaVu Sans Mono" :size 12))
-    ;;(font-spec :family "Hiragino Sans GB" ))
+   (frame-parameter nil 'font)
+   'han
+   (font-spec :family "STHeiti" :size 15))
+  ;;(font-spec :name "WenQuanYi Micro Hei Mono"))
+  ;;(font-spec :family "DejaVu Sans Mono" :size 12))
+  ;;(font-spec :family "Hiragino Sans GB" ))
   )
+
+(defun my-http-proxy()
+  (setq url-proxy-services
+        '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+          ("http" . "proxy.com:8080")
+          ("https" . "proxy.com:8080")))
+
+  (setq url-http-proxy-basic-auth-storage
+        (list (list "proxy.com:8080"
+                    (cons "Input your LDAP UID !"
+                          (base64-encode-string "LOGIN:PASSWORD"))))))
 
 (defun my-open-finder-1 (dir file)
   (let ((script
-		 (if file
-			 (concat
-			  "tell application \"Finder\"\n"
-			  "    set frontmost to true\n"
-			  "    make new Finder window to (POSIX file \"" dir "\")\n"
-			  "    select file \"" file "\"\n"
-			  "end tell\n")
-		   (concat
-			"tell application \"Finder\"\n"
-			"    set frontmost to true\n"
-			"    make new Finder window to {path to desktop folder}\n"
-			"end tell\n"))))
+         (if file
+             (concat
+              "tell application \"Finder\"\n"
+              "    set frontmost to true\n"
+              "    make new Finder window to (POSIX file \"" dir "\")\n"
+              "    select file \"" file "\"\n"
+              "end tell\n")
+           (concat
+            "tell application \"Finder\"\n"
+            "    set frontmost to true\n"
+            "    make new Finder window to {path to desktop folder}\n"
+            "end tell\n"))))
     (start-process "osascript-getinfo" nil "osascript" "-e" script)))
 
 
 (defun my-open-finder ()
   (interactive)
   (let ((path (buffer-file-name))
-		dir file)
-	(when path
-	  (setq dir (file-name-directory path))
-	  (setq file (file-name-nondirectory path)))
-	(my-open-finder-1 dir file)))
+        dir file)
+    (when path
+      (setq dir (file-name-directory path))
+      (setq file (file-name-nondirectory path)))
+    (my-open-finder-1 dir file)))
 
 ;; C-x C-e ;; current line
 ;; M-x eval-region ;; region
