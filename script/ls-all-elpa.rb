693862aa0fd446d36e7c75b1a705c9b002e90340
zhijia,.zhang <jiahut@gmail.com>
2015-01-25 18:16:22 +0800

    update script

--- script/ls-all-elpa.rb
+++ script/ls-all-elpa.rb
@@ -1,9 +1,31 @@
 #!/usr/bin/env ruby
 
-ls = `ls -1 ../elpa`
+ls = `ls -1 ../elpa/`
+elpas = {}
 
 ls.split.each do |line|
-  if (match = line.match /([\w|-]+)-[\d.]+/)
-    puts match[1]
+  if (match = line.match /^([\w|-]+[+]*?)-([\d.]+)$/)
+    # puts "#{match[1]}-#{match[2]}"
+    tmp = elpas[match[1]]
+    if tmp
+      tmp << match[2]
+    else
+      elpas[match[1]] = [ match[2] ]
+    end
   end
 end
+
+overdue = []
+
+elpas.each_entry do | k,v | 
+  if v.length > 1 
+    overdue <<  [k,v.min].join("-")
+  end
+end
+
+overdue.each do  | elpa | 
+  puts elpa
+  # `rm -rf ../elpa/#{elpa}`
+end
+
+# ./ls-all-elpa.rb | xargs rm -rf 
